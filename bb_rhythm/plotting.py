97bf4403937302574dcd0d8b681bc714abbafb12
JuliaMelle01 <julia.mellert@gmx.de>
2023-06-22 14:54:42 +0200

    stuff for plot 2

--- bb_rhythm/plotting.py
+++ bb_rhythm/plotting.py
@@ -1,12 +1,15 @@
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import numpy.ma as ma
 import datetime
 from scipy.ndimage import gaussian_filter1d
 
 from . import utils
+from . import rhythm
 
 
 def plot_body_location_of_interactions(vel_change_matrix_df, plot_dir=None, imshow=False, annotate=False, ax=None, title_extra=None):
@@ -213,3 +216,171 @@ def round_time_age_velocity_df(round_time, time_age_velocity_df):
     time_age_velocity_df["date"] = time_age_velocity_df['date'].dt.round(round_time)
     time_age_velocity_df = time_age_velocity_df.groupby(["date", "age"])['velocity'].mean().reset_index()
     return time_age_velocity_df
+
+
+def plot_boxplot(circadianess_df, ax, x="age_bins"):
+    sns.boxplot(circadianess_df,
+                ax=ax,
+                x=x,
+                y="mean",
+                width=0.5,
+                flierprops={"marker": "o", "color": (0, 0, 0, 0)},
+                boxprops={"facecolor": (0, 0, 0, 0), "edgecolor": "blue"},
+                whiskerprops={"color": "blue"},
+                medianprops={"color": "green", "linewidth": 1},
+                showfliers=True,
+                capprops={"color": "blue"}
+            )
+
+
+def set_fig_props_circadianess_per_age_plot(fig):
+    fig.supxlabel('Age [days]')
+    fig.supylabel('')
+    fig.suptitle('Fraction of bees sig. different from shuffled data\n and the shuffled data was chi²')
+
+
+def plot_raincloudplot(circadianess_df, ax, x="age_bins", hue_norm=None):
+    # Create violin plots without mini-boxplots inside.
+    sns.violinplot(data=circadianess_df, x=x, y="mean", color='mediumslateblue', cut=0, inner=None, ax=ax)
+
+    # Clip the lower half of each violin.
+    for item in ax.collections:
+        x0, y0, width, height = item.get_paths()[0].get_extents().bounds
+        item.set_clip_path(plt.Rectangle((x0, y0), width/2, height,#/2,
+                           transform=ax.transData))
+
+    # Create strip plots with partially transparent points of different colors depending on the group.
+    num_items = len(ax.collections)
+    sns.stripplot(data=circadianess_df, x=x, y="mean", hue="count", size=7, ax=ax, hue_norm=hue_norm)
+
+    # Shift each strip plot strictly below the correponding volin.
+    for item in ax.collections[num_items:]:
+       for pos in item.get_offsets():
+          pos[0] = pos[0] + 0.125
+
+    # Create narrow boxplots on top of the corresponding violin and strip plots, with thick lines, the mean values, without the outliers.
+    sns.boxplot(data=circadianess_df, x=x, y="mean", width=0.25,
+                showfliers=False, showmeans=True,
+                meanprops=dict(markerfacecolor='lightgreen',
+                               markersize=5, zorder=3),
+                boxprops=dict(facecolor=(0,0,0,0),
+                              linewidth=3, zorder=3),
+                whiskerprops=dict(linewidth=3),
+                capprops=dict(linewidth=3),
+                medianprops=dict(linewidth=3), ax=ax)
+
+
+def plot_violin_swarm_plot(circadianess_df, ax, x="age_bins", size_norm=None, date_ann=False, count_ann=False):
+    # get coordinates from swarm plot
+    sns.swarmplot(data=circadianess_df, x=x, y="mean", ax=ax, size=7.)
+    coordinates = [ma.getdata(ax.collections[i].get_offsets()) for i in range(len(ax.collections))]
+    ax.clear()
+
+    # plot violins
+    sns.violinplot(data=circadianess_df, x=x, y="mean", palette=["lightgrey"], cut=0, saturation=0.5, scale="width", orient='v', ax=ax, inner="quartile", alpha=0.5)
+    circadianess_df['count'][circadianess_df['count'] == "nan"] = 0.
+
+    # plot scatter
+    if date_ann:
+        sns.scatterplot(data=circadianess_df, x=x, y="mean", size="count", hue="date", palette="viridis", size_norm=size_norm, ax=ax)
+    else:
+        sns.scatterplot(data=circadianess_df, x=x, y="mean", size="count", size_norm=size_norm, ax=ax)
+
+    # annotate number of counts
+    if count_ann:
+        for x, y, count in zip(circadianess_df[x], circadianess_df["mean"], circadianess_df["count"]):
+            ax.annotate(count, xy=(x, y))
+
+    # use coordinates from swarm plot to spread scatters
+    ax_index = find_ax_collection_for_plotting(ax)
+    ax.collections[ax_index].set_offsets([item for sublist in coordinates for item in sublist])
+
+
+def find_ax_collection_for_plotting(ax):
+    ax_index = 0
+    for i in range(len(ax.collections)):
+        if ax.collections[i].get_offsets().size != 0:
+            ax_index = i
+    return ax_index
+
+
+def plot_circadianess_per_age_group(circadianess_df, plot_type='boxplot', young=False, file_path=None, age_map_step_size=5, age_bins=None):
+    # calculate well tested circadianess
+    rhythm.calculate_well_tested_circadianess(circadianess_df)
+
+    # create age map and bins
+    utils.add_age_bins(circadianess_df, age_map_step_size, age_bins)
+    circadianess_df = circadianess_df[circadianess_df["age_bins"] != "Nan"]
+
+    circadianess_df_young = None
+    if young:
+        # get subset of young bees
+        circadianess_df_young = circadianess_df[circadianess_df["age"] <= 5]
+
+        # Create dataframe with aggregated mean and count of well tested circadianess per day
+        circadianess_df_young = rhythm.create_mean_count_circadianess_per_day_df(circadianess_df_young, column="age")
+
+    # Create dataframe with aggregated mean and count of well tested circadianess per day
+    circadianess_df = rhythm.create_mean_count_circadianess_per_day_df(circadianess_df)
+
+    # get count norm for shared legend
+    min_count = circadianess_df["count"].min()
+    max_count = circadianess_df["count"].max()
+
+    plot_agg_well_tested_circadianess_per_bee_age(circadianess_df, circadianess_df_young, max_count, min_count,
+                                                  plot_type, young, file_path)
+
+
+def plot_agg_well_tested_circadianess_per_bee_age(circadianess_df, circadianess_df_young, max_count=None, min_count=None, plot_type="boxplot", young=False, file_path=None):
+    if young:
+        # create figure and subplots
+        fig, (ax0, ax1) = plt.subplots(1, 2, sharey=True, gridspec_kw={'width_ratios': [3, 7]}, figsize=(16, 8))
+
+        # plot
+        plot_agg_circadianess_according_plot_type(ax=ax0, circadianess_df=circadianess_df_young, norm=(min_count, max_count), plot_type=plot_type, x="age")
+
+        # set axis properties
+        if ax0.get_legend():
+            ax0.legend().set_visible(False)
+        set_ax_props_circadianess_per_age_group_plot(ax0)
+
+    else:
+        # create figure and subplots
+        fig, ax1 = plt.subplots(1, 1, figsize=(15, 10))
+
+    # plot
+    plot_agg_circadianess_according_plot_type(ax=ax1, circadianess_df=circadianess_df, norm=(min_count, max_count), plot_type=plot_type)
+
+    # set axis properties
+    if ax1.get_legend():
+        ax1.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, title='Samples per day')
+    set_ax_props_circadianess_per_age_group_plot(ax1)
+
+    # set label properties
+    set_fig_props_circadianess_per_age_plot(fig)
+
+    # set plt properties
+    plt.ylim(0., 1.1)
+    plt.tight_layout()
+    if file_path:
+        plt.savefig(file_path)
+
+
+def plot_agg_circadianess_according_plot_type(ax=None, circadianess_df=None, norm=None, plot_type="boxplot", x="age_bins", date_ann=False, count_ann=False):
+    if plot_type == "boxplot":
+        plot_boxplot(circadianess_df, x=x, ax=ax)
+    elif plot_type == "violin_swarm":
+        plot_violin_swarm_plot(circadianess_df, ax=ax, x=x, size_norm=norm, date_ann=date_ann, count_ann=count_ann)
+    elif plot_type == "raincloud":
+        plot_raincloudplot(circadianess_df, ax=ax, x=x, hue_norm=norm)
+    else:
+        raise ValueError("Incorrect plot type. Possible types: ['boxplot', 'violin_swarm', 'raincloud']")
+
+
+def set_ax_props_circadianess_per_age_group_plot(ax):
+    # set step size ax ticks
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+
+    # set ax labels
+    ax.set_xlabel('')
+    ax.set_ylabel('')
